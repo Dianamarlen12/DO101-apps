27006076ac5740876eee5c05be644739be81c679
Dianamarlen12 <diana.meneses@ids.com.mx>
2022-06-29 18:19:45 -0500

    contacts

--- contacts/db/config.js
+++ contacts/db/config.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+/**Configuración a la base de datos */
 const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://contacts:contacts@contactsdb:5432/contactsdb';
 const { Pool } = require('pg');
 
